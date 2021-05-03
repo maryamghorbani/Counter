971550a4f5e3e739ead8c1659cdbae4b6ccf0948
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-03 14:37:02 +0430

    set timer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ let inputCounter = startBox.querySelector('#input-counter');
 let startCounter = startBox.querySelector('#start-counter');
 let errorElement = document.querySelector('#error-message');
 let timerCircle = document.querySelector('.c100');
+let timerNum = document.querySelector('.c100 > span');
+let loadingMessage = document.querySelector('.message .loading');
+let successMessage = document.querySelector('.message .success');
 
 
 startCounter.addEventListener('click' , function (e) {
@@ -18,4 +21,23 @@ startCounter.addEventListener('click' , function (e) {
     errorElement.classList.remove('active');
     startBox.style.display = 'none';
     timerCircle.style.display = 'block';
+    timerNum.textContent = secondsValue;
+    loadingMessage.style.display = 'block';
+    successMessage.style.display = 'none';
+
+
+    let timeriId = setInterval(() => {
+
+        if ( secondsValue<= 1 ) {
+            clearInterval(timeriId)
+            startBox.style.display = 'block';
+            timerCircle.style.display = 'none';
+            loadingMessage.style.display = 'none';
+            successMessage.style.display = 'block';
+            inputCounter.value = ''
+        }
+
+        secondsValue  -= 1;
+        timerNum.textContent = secondsValue;
+        },1000);
 })
