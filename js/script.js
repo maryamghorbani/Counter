ea0f4c29f306a52620b40b7cf9562b46ffe7fa05
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-03 17:17:28 +0430

    refactor codes

--- js/script.js
+++ js/script.js
@@ -1,49 +1,85 @@
 let startBox = document.querySelector('.start-box');
 let inputCounter = startBox.querySelector('#input-counter');
 let startCounter = startBox.querySelector('#start-counter');
-let errorElement = document.querySelector('#error-message');
 let timerCircle = document.querySelector('.c100');
 let timerNum = document.querySelector('.c100 > span');
-let loadingMessage = document.querySelector('.message .loading');
-let successMessage = document.querySelector('.message .success');
+let lastPercent = 'p100';
+let originalSecond, secondsValue, timeriId;
+
 
 
 startCounter.addEventListener('click' , function (e) {
-    let secondsValue = parseInt(inputCounter.value)
+    secondsValue = parseInt(inputCounter.value)
     console.log(secondsValue)
 
-    if (isNaN(secondsValue)) {
-        errorElement.textContent = 'Inter a number, please!';
-        errorElement.classList.add('active')
-        return
+    if (isNaN(secondsValue)) return toggleErrorMessage({show: true , message: 'Input valid number, please!'});
+
+    toggleErrorMessage({ show: false });
+    toggleStartBox({ show: false });
+    toggleLoadingMessage({ show: true });
+    toggleTimer({ show: true }, secondsValue)
+
+
+    originalSecond = secondsValue;
+    timeriId = setInterval(startTimer,1000);
+})
+
+let startTimer = () => {
+    if(lastPercent) timerCircle.classList.remove(lastPercent);
+    if ( secondsValue<= 0 ) {
+        clearInterval(timeriId)
+        toggleStartBox({ show: true });
+        toggleTimer({ show:false });
+        toggleLoadingMessage({ show: false });
+        return;
     }
 
-    errorElement.classList.remove('active');
-    startBox.style.display = 'none';
-    timerCircle.style.display = 'block';
+
+    secondsValue  -= 1;
     timerNum.textContent = secondsValue;
-    loadingMessage.style.display = 'block';
-    successMessage.style.display = 'none';
 
-    let originalSecond = secondsValue;
-    let lastPercent = 'p100'
-    let timeriId = setInterval(() => {
-        if(lastPercent) timerCircle.classList.remove(lastPercent);
-        if ( secondsValue<= 0 ) {
-            clearInterval(timeriId)
-            startBox.style.display = 'block';
-            timerCircle.style.display = 'none';
-            loadingMessage.style.display = 'none';
-            successMessage.style.display = 'block';
-            inputCounter.value = '';
-            return;
-        }
+    let percent = lastPercent = `p${Math.abs(Math.floor( ((originalSecond - secondsValue)/originalSecond) * 100) - 100 )}`
+    timerCircle.classList.add(percent)
+
+}
 
 
-        secondsValue  -= 1;
-        let percent = Math.abs(Math.floor( ((originalSecond - secondsValue)/originalSecond) * 100) - 100 )
-        lastPercent = `p${percent}`;
-        timerCircle.classList.add(`p${percent}`)
+let toggleErrorMessage = ({show, message}) => {
+    let errorElement = document.querySelector('#error-message');
+    if(show) {
+        errorElement.textContent = message;
+        errorElement.classList.add('active');
+    } else {
+        errorElement.classList.remove('active');
+    }
+}
+
+let toggleStartBox = ({show}) => {
+    if( show ) {
+        startBox.style.display = 'block';
+        inputCounter.value = '';
+    } else {
+        startBox.style.display = 'none';
+    }
+}
+
+let toggleLoadingMessage = ({ show }) => {
+    let loadingMessage = document.querySelector('.message .loading');
+    let successMessage = document.querySelector('.message .success');
+    if(show) {
+        loadingMessage.style.display = 'block';
+        successMessage.style.display = 'none';
+    }else {
+        loadingMessage.style.display = 'none';
+        successMessage.style.display = 'block';
+    }
+}
+
+let toggleTimer = ({ show, secondsValue }) => {
+    if (show) {
+        timerCircle.style.display = 'block';
         timerNum.textContent = secondsValue;
-        },1000);
-})
+    } else {
+        timerCircle.style.display = 'none';
+    }
+}
